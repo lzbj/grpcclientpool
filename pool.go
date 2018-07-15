d33e45aed9dbb1b7547569f10ead3ac885d264de
zhaoliu <zhaoliu@freewheel.tv>
2018-07-15 08:54:53 +0800

    Refactor with perf test.

--- pool.go
+++ pool.go
@@ -10,7 +10,7 @@ import (
 	"wheels/log"
 )
 
-var Debug = true
+var debug = false
 
 // ErrorPoolSize means the initial size is wring
 var ErrorPoolSize = errors.New("the size of grpc connection pool should be greater than 0")
@@ -30,8 +30,12 @@ var ErrorConnAlreadyClosed = errors.New("the grpc connection is closed")
 // ErrorCloseConn means error happened during close grpc conn.
 var ErrCloseConn = errors.New("error happened during close grpc client connection")
 
-// ErrorCreateConn means error happened during create grpc conn.
-var ErrConnCreation = errors.New("error happened during create grpc client connection")
+var addr string
+var options []grpc.DialOption
+
+var reuseQuota int32 = 100000
+var counter int32
+var globalconn = &grpcConn{}
 
 // Grpc connection pool
 type GrpcConnPool struct {
@@ -55,7 +59,7 @@ type grpcConn struct {
 }
 
 // NewGrpcClientConnPool creates a pool holds a bunch of live grpc client conns.
-func NewGrpcConnPool(creator ClientConCreator, size int32, idleTimeout time.Duration, maxLifeDuration time.Duration) (*GrpcConnPool, error) {
+func NewGrpcConnPool(creator ClientConCreator, size int, idleTimeout time.Duration, maxLifeDuration time.Duration) (*GrpcConnPool, error) {
 	if size <= 0 {
 		return nil, ErrorPoolSize
 	}
@@ -67,12 +71,11 @@ func NewGrpcConnPool(creator ClientConCreator, size int32, idleTimeout time.Dura
 		maxLifeDuration: maxLifeDuration,
 	}
 
-	var i int32
-	for i = 0; i < size; i++ {
+	for i := 0; i < size; i++ {
 
 		cliconn, err := creator()
 		if err != nil {
-			if Debug {
+			if debug {
 				log.Debug("error happened during create grpc client connection")
 				log.Errorf("%v", err)
 				//Insert some placeholder
@@ -91,33 +94,62 @@ func NewGrpcConnPool(creator ClientConCreator, size int32, idleTimeout time.Dura
 
 	}
 
-	if Debug {
+	if debug {
 		log.Debug("created a pool with the size %d ", size)
 	}
 	return pool, nil
 }
 
 // Size return the size of the pool.
-func (p *GrpcConnPool) Size() int32 {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	size := int32(cap(p.conns))
+func (p *GrpcConnPool) Size() int {
 
 	if p.PoolIsClosed() {
 		return 0
 	}
-	return atomic.LoadInt32(&size)
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	return cap(p.conns)
 
 }
 
 // AvailableGrpcClientConn returns the available conns.
-func (p *GrpcConnPool) Available() int32 {
+func (p *GrpcConnPool) Available() int {
 
 	if p.PoolIsClosed() {
 		return 0
 	}
-	avai := int32(len(p.conns))
-	return atomic.LoadInt32(&avai)
+	return len(p.conns)
+}
+
+func (p *GrpcConnPool) GetReuse(ctx context.Context) (*grpcConn, error) {
+	if globalconn != nil && globalconn.ClientConn != nil {
+		return globalconn, nil
+	}
+	con, err := p.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	atomic.AddInt32(&counter, 1)
+	if counter <= reuseQuota {
+		globalconn = con
+		return globalconn, nil
+	}
+	//exceeds the reuse limit
+	//put the old back and create new one
+	atomic.AddInt32(&counter, -reuseQuota)
+	err = globalconn.Close()
+	if err != nil {
+		return nil, err
+	}
+	//get a new one
+	con, err = p.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	atomic.AddInt32(&counter, 1)
+	globalconn = con
+	return globalconn, err
+
 }
 
 // GetGrpcCliCon returns a *grpcClientConn instance.
@@ -148,7 +180,7 @@ func (p *GrpcConnPool) Get(ctx context.Context) (*grpcConn, error) {
 		go func() (*grpcConn, error) {
 			wraper.ClientConn, err = p.creator()
 			if err != nil {
-				if Debug {
+				if debug {
 					log.Debug("error happened during create grpc client connection")
 				}
 				select {
@@ -193,9 +225,7 @@ func (p *GrpcConnPool) ClosePool() error {
 	}
 
 	close(conns)
-	var i int32
-	for i = 0; i < p.Available(); i++ {
-		con := <-conns
+	for con := range conns {
 		if con.ClientConn == nil {
 			continue
 		}
@@ -248,4 +278,21 @@ func (cc *grpcConn) Close() error {
 
 	cc.ClientConn = nil
 	return nil
+}
+
+func NewSyncPool(address string, opts ...grpc.DialOption) *sync.Pool {
+	addr = address
+	options = opts
+	p := &sync.Pool{
+		New: CreateConn,
+	}
+	return p
+}
+
+func CreateConn() interface{} {
+	con, err := grpc.Dial(addr, options...)
+	if err != nil {
+		return nil
+	}
+	return con
 }
